0a165e34ba3bf646a2048d432d77c2c40e6519a0
Tạ Hữu Cường <dragonsvip090@gmail.com>
2026-03-27 10:48:41 +0700

    chore: test CI/CD by making a safe change

--- auction-client/src/main/java/com/auctionuet/client/Launcher.java
+++ auction-client/src/main/java/com/auctionuet/client/Launcher.java
@@ -1,6 +1,7 @@
 package com.auctionuet.client;
 
 public class Launcher {
+    // This is a test comment for CI/CD verification.
     public static void main(String[] args) {
         ClientApp.main(args);
     }
