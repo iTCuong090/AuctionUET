a1edc750b6317db80347d55265f95b646fd56605
Tạ Hữu Cường <dragonsvip090@gmail.com>
2026-04-11 22:28:07 +0700

    tam thoi xong tuan 2-3

--- auction-server/src/main/java/com/auctionuet/server/persistence/schema/UserRole.java
+++
@@ -1,9 +0,0 @@
-package com.auctionuet.server.persistence.schema;
-
-//File siêu tạm thời, cần phải bị xóa và import gói Enums của người khác vào dự án.
-
-public enum UserRole {
-    BIDDER,
-    SELLER,
-    ADMIN
-}
