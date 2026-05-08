b7ded84d44f5f4d4eba1956e5498f92061524c5e
25020046-nguyencaocong <25020046@vnu.edu.vn>
2026-05-08 16:41:26 +0700

    hoan thanh gd2 tuan4 gui

--- auction-client/src/main/java/com/auctionuet/client/network/ItemClient.java
+++ auction-client/src/main/java/com/auctionuet/client/network/ItemClient.java
@@ -3,7 +3,6 @@ package com.auctionuet.client.network;
 import com.auctionuet.client.model.ItemDTO;
 import com.auctionuet.client.network.protocol.Request;
 import com.auctionuet.client.network.protocol.Response;
-// IMPORT CÁI ENUM NÀY VÀO LÀ HẾT ĐỎ NÈ
 import com.auctionuet.client.network.protocol.ActionType;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -15,29 +14,34 @@ public class ItemClient {
     private Gson gson = new Gson();
 
     public ItemDTO createItem(String token, Map<String, Object> itemData) throws Exception {
-        // THAY VÌ DÙNG "CREATE_ITEM", TA DÙNG ENUM ActionType.CREATE_ITEM
         Request request = new Request(ActionType.CREATE_ITEM, itemData);
         request.setToken(token);
 
         Response response = ServerConnection.getInstance().sendRequest(request);
 
         if ("OK".equals(response.getStatus())) {
-            return gson.fromJson(response.getData().toString(), ItemDTO.class);
+            // ĐÃ FIX: Ép cục data thành chuỗi JSON chuẩn (có dấu ngoặc kép)
+            String jsonCorrect = gson.toJson(response.getData());
+
+            // Sau đó mới cho máy Gson đổ khuôn
+            return gson.fromJson(jsonCorrect, ItemDTO.class);
         } else {
             throw new Exception(response.getMessage());
         }
     }
 
     public List<ItemDTO> getMyItems(String token) throws Exception {
-        // TƯƠNG TỰ BÊN NÀY
         Request request = new Request(ActionType.GET_MY_ITEMS, null);
         request.setToken(token);
 
         Response response = ServerConnection.getInstance().sendRequest(request);
 
         if ("OK".equals(response.getStatus())) {
+            // ĐÃ FIX TƯƠNG TỰ Ở ĐÂY
+            String jsonCorrect = gson.toJson(response.getData());
+
             Type listType = new TypeToken<List<ItemDTO>>(){}.getType();
-            return gson.fromJson(response.getData().toString(), listType);
+            return gson.fromJson(jsonCorrect, listType);
         } else {
             throw new Exception(response.getMessage());
         }
